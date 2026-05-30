2f1e21b61d68edd32dd8cfde7a690d9b6cb388a7
uberpatryck9111 <uberpatryck9111@gmail.com>
2026-05-30 14:32:53 -0400

    solved weather 5

---
+++ sqlcity.py
@@ -0,0 +1,11 @@
+(SELECT city, LENGTH(city)
+FROM station 
+ORDER BY LENGTH(city) ASC, city ASC 
+LIMIT 1)
+
+UNION 
+
+(SELECT city, LENGTH(city)
+FROM station 
+ORDER BY LENGTH(city) DESC, city ASC 
+LIMIT 1)
